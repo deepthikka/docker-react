c075da41dff73b3c3ffe1aeb680cb91947f77876
Deepthikka <Deepthikka@NBEC-1VV4ZM2.nectechnologies.in>
2020-05-30 00:05:04 +0530

    Modified App.js for feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Congratulations Deepthikka on your first AWS deployment!!!
+          Congratulations Deepthikka on your first AWS deployment!!! Now in feature branch!!!
         </p>
         <a
           className="App-link"
